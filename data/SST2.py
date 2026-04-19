ff160dc36b07b6a1d823be37e1507fc8fe753c13
carlyhcy <carlyh1211@gmail.com>
2026-04-18 21:49:17 -0400

    changed model_name into a parameter so we can test different models

--- data/SST2.py
+++ data/SST2.py
@@ -3,7 +3,7 @@ from transformers import AutoTokenizer, DataCollatorWithPadding
 
 def load_sst2_data(model_name="distilbert-base-uncased", max_length=128):
     dataset = load_dataset("glue", "sst2")
-    tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
     def preprocess(example):
         return tokenizer(example["sentence"], truncation=True, max_length=128)
     
