248eb8db1c7133c4bff0969473ab5eb2ab00e3c4
carlyhcy <carlyh1211@gmail.com>
2026-04-18 21:41:46 -0400

    turn the codes into function

--- data/IMDB.py
+++ data/IMDB.py
@@ -4,16 +4,18 @@ from transformers import (
     DataCollatorWithPadding
 )
 
-dataset = load_dataset("imdb")
-tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
-def preprocess(example):
-    return tokenizer(
-        example["text"],
-        truncation=True,
-        max_length=256
-    )
-
-dataset = dataset.map(preprocess, batched=True)
-dataset = dataset.remove_columns(["text"])
-dataset.set_format("torch")
-data_collator = DataCollatorWithPadding(tokenizer) # this is the tokenized data
+def load_sst2_data(model_name="distilbert-base-uncased", max_length=128):
+    dataset = load_dataset("imdb")
+    tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+    def preprocess(example):
+        return tokenizer(
+            example["text"],
+            truncation=True,
+            max_length=256
+        )
+    
+    dataset = dataset.map(preprocess, batched=True)
+    dataset = dataset.remove_columns(["text"])
+    dataset.set_format("torch")
+    data_collator = DataCollatorWithPadding(tokenizer) # this is the tokenized data
+    return dataset, tokenizer, data_collator
