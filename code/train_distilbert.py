6885fa7bb14849a56e195c3386e083b2e63c1040
carlyhcy <carlyh1211@gmail.com>
2026-04-21 11:07:51 -0400

    updated trainer and training argument to fit the new version of transformers

--- code/train_distilbert.py
+++ code/train_distilbert.py
@@ -59,7 +59,7 @@ def main():
         args=training_args,
         train_dataset=dataset["train"],
         eval_dataset=dataset["validation"],
-        tokenizer=tokenizer,
+        processing_class=tokenizer,
         data_collator=data_collator,
         compute_metrics=compute_metrics
     )
